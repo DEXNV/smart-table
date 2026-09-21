11d1c815a80b907ed22f0d0012215c6b93c06599
DEXNV <dex.nv@yandex.ru>
2026-09-21 22:50:30 +0500

    Update 1.0 Fixed func &more

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
-// @todo: подключение
+import {initSorting} from "./components/sorting.js"
 import {initPagination} from "./components/pagination.js";
+import {initFiltering} from "./components/filtering.js";
+import {initSearching} from "./components/searching.js";
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -37,7 +39,10 @@ function collectState() {
 function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+
+    result = applySearching(result, state, action);
+    result = applyFiltering(result, state, action); 
+    result = applySorting(result, state, action); 
     result = applyPagination(result, state, action); 
 
     sampleTable.render(result)
@@ -46,9 +51,20 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
+    before: ['search', 'header', 'filter'],
     after: ['pagination']
 }, render);
+
+const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
+    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
+}); 
+
+const applySearching = initSearching(sampleTable.search.elements, 'search');
 
 // @todo: инициализация
 const applyPagination = initPagination(
